9846c95e61d5f9117e7ce5bae80f4bbbac962715
Bulisz <bulisz@freemail.hu>
2023-09-30 15:17:06 +0200

    auto deploy w/o self-contained, Data folder

--- MohaQuiz.Frontend/src/environments/environment.ts
+++ MohaQuiz.Frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://pn2000jan01-001-site1.ctempurl.com/mohaquiz/',
-    hubUrl: 'https://pn2000jan01-001-site1.ctempurl.com/gamecontrolhub'
+    apiUrl: 'https://mohaquiz1-001-site1.etempurl.com/mohaquiz/',
+    hubUrl: 'https://mohaquiz1-001-site1.etempurl.com/gamecontrolhub'
     // apiUrl: 'http://86.101.113.227:80/mohaquiz/',
     // hubUrl: 'http://86.101.113.227:80/gamecontrolhub'
 };
